589df4aa258b80a8133b157d13e019999a9f6183
Dean Kago <martokhago@gmail.com>
2022-04-24 15:00:59 +0300

    added user class

---
+++ user.py
@@ -0,0 +1,20 @@
+from psutil import users
+from requests import delete
+
+
+class User:
+  userslist=[]
+  def __init__(self, firstname, lastname, username,password):
+    self.firstname = firstname
+    self.lastname = lastname
+    self.username = username
+    self.password = password
+
+  def save_user(self):
+    User.userslist.append(self)
+
+  def delete_user(self):
+    User.userslist.remove(self)
+  # @classmethod
+
+  # def show_users(cls)  
