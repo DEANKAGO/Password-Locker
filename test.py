f261901145aa6c377b47087c0d406a2018b90720
Dean Kago <martokhago@gmail.com>
2022-04-24 18:08:07 +0300

    created contact object

--- test.py
+++ test.py
@@ -3,3 +3,9 @@ from user import User
 from credentials import Credentials
 
 class Testuser(unittest.TestCase):
+
+  def setUp(self):
+    self.new_user = User("Martin", "Kago", "Python")
+
+  def test_init(self):
+    self.assertEqual()  
